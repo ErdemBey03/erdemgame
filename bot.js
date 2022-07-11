f10ea99e47244713c250c43f4d0248197660db8d
Erdem <109048333+ErdemBey03@users.noreply.github.com>
2022-07-11 12:51:52 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,11 @@
 const { Telegraf,Telegram } require('Telegraf')
-const config = require('./config')
-const telegram = new Telegrsm
+const config = require("./config")
+const telegram = new Telegram(config.token)
+const bot = new Telegraf(config.token)
+
+
+
+
+
+process.once('SIGNINT', () => bot.stop('SIGNINT'))
+process.once('SIGNTERM', () => bot.stop('SIGNTERM'))
